93bc3b7379c748a16eb9d1bd0806207adc18b12c
Valkary <pepoclesng@gmail.com>
2023-07-14 11:05:15 -0600

    solo solo sanborns

--- src/components/Departamentos/Departamentos.tsx
+++ src/components/Departamentos/Departamentos.tsx
@@ -1,15 +1,17 @@
 import { departamentos } from "../../types/departamentos";
 import DetailDepartamento from "./DetailDepartamento";
 
-export default function Departamentos({}) {
+export default function Departamentos({ }) {
     const entries = Object.entries(departamentos);
 
-    return <div
+    return (
+        <div
             className="
-                w-full max-w-full flex flex-col justify-center items-center
-                lg:max-w-7xl
-            "
+                    w-full max-w-full flex flex-col justify-center items-center my-10
+                    lg:max-w-7xl lg:flex-row lg:flex-wrap lg:gap-4 lg:justify-evenly
+                "
         >
-        {entries.map((entry, id) => <DetailDepartamento key={id} departamento={entry[1]} section={entry[0]} />)}
-    </div>
+            {entries.map((entry, id) => <DetailDepartamento key={id} departamento={entry[1]} section={entry[0]} />)}
+        </div>
+    )
 }
